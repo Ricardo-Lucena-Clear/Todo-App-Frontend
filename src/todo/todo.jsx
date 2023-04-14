c9ed954793f6a3a6790fe5929b15ead6ee9a3562
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-14 17:36:01 -0300

    TODO search

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -12,7 +12,7 @@ export default class Todo extends Component {
     constructor(props){
         super(props)
         this.state = {description: "", list: []}
-        
+        this.handleSearch = this.handleSearch.bind(this)
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
         this.handleChange = this.handleChange.bind(this)
@@ -20,12 +20,16 @@ export default class Todo extends Component {
         this.handleRemove = this.handleRemove.bind(this)
         this.refresh()
     }
-    refresh(){
-        axios.get(`${URL}?sort=-createAt`)
-        .then(resp => this.setState({...this.state, description: "", list: resp.data}))
+    refresh(description = ""){
+        const search = description ? `&description__regex=/${description}/`:""
+        axios.get(`${URL}?sort=-createAt${search}`)
+        .then(resp => this.setState({...this.state, description, list: resp.data}))
+    }
+    handleSearch(){
+        this.refresh(this.state.description)
     }
     handleRemove(todo){
-        axios.delete(`${URL}/${todo._id}`).then(resp => this.refresh())
+        axios.delete(`${URL}/${todo._id}`).then(resp => this.refresh(this.state.description))
     }
 
     handleAdd(){
@@ -38,11 +42,11 @@ export default class Todo extends Component {
     }
     handleMarkAsDone(todo){
         axios.put(`${URL}/${todo._id}`, {...todo, done: true})
-        .then(resp => this.refresh())
+        .then(resp => this.refresh(this.state.description))
     }
     handleMarkAsPending(todo){
         axios.put(`${URL}/${todo._id}`, {...todo, done: false})
-        .then(resp => this.refresh())
+        .then(resp => this.refresh(this.state.description))
     }
     render() {
         return (
@@ -50,7 +54,8 @@ export default class Todo extends Component {
                 <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
                 <TodoForm description={this.state.description}
                 handleChange={this.handleChange}
-                handleAdd={this.handleAdd}/>
+                handleAdd={this.handleAdd}
+                handleSearch={this.handleSearch}/>
                 <TodoList list={this.state.list}
                 handleRemove={this.handleRemove}
                 handleMarkAsDone={this.handleMarkAsDone}
