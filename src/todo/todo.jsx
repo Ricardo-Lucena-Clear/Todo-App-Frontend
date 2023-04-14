5f74d096db365de276248e26ba06840fd9a8c2f0
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-14 16:44:01 -0300

    Consultation and Deletion of TODOs

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -15,12 +15,21 @@ export default class Todo extends Component {
 
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.refresh()
+    }
+    refresh(){
+        axios.get(`${URL}?sort=-createAt`)
+        .then(resp => this.setState({...this.state, description: "", list: resp.data}))
+    }
+    handleRemove(todo){
+        axios.delete(`${URL}/${todo._id}`).then(resp => this.refresh())
     }
 
     handleAdd(){
         const description = this.state.description
         axios.post(URL, {description})
-        .then(resp => console.log('Funcionou'))
+        .then(resp => this.refresh())
     }
     handleChange(e){
         this.setState({...this.state, description: e.target.value})
@@ -32,7 +41,8 @@ export default class Todo extends Component {
                 <TodoForm description={this.state.description}
                 handleChange={this.handleChange}
                 handleAdd={this.handleAdd}/>
-                <TodoList/>
+                <TodoList list={this.state.list}
+                handleRemove={this.handleRemove}/>
             </div>
         )
     }
